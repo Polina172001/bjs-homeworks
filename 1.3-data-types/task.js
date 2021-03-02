0585bf415532056c176fcd4331940fd98924bd60
Polina Filippova <polina17012001@mail.ru>
2021-03-02 19:41:15 +0300

    Added task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,24 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let s = amount - contribution; //сумма кредита - первонач взнос
+    let p = percent / 100 / 12;
+    let n = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
+
+    let monthly_payment = s * (p + (p / (Math.pow(1 + p, n) - 1)));
+    
+    let totalAmount = monthly_payment * n;
+    console.log(totalAmount.toFixed(2));
+    return totalAmount.toFixed(2);
 }
 
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    if (name === undefined || name === null || name === "") {
+        greeting = 'Привет, мир! Меня зовут Аноним.';
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+    }
+    console.log(greeting);
+    return greeting;
 }
