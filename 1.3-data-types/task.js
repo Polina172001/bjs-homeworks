0e8d2ca9cf369071f66c03ea67c3fb7f0be8ef61
Polina Filippova <polina17012001@mail.ru>
2021-03-05 15:36:03 +0300

    Added-2

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,23 +1,42 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    let s = amount - contribution; //сумма кредита - первонач взнос
-    let p = percent / 100 / 12;
-    let n = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
-
-    let monthly_payment = s * (p + (p / (Math.pow(1 + p, n) - 1)));
+    if (isNaN(percent) || percent < 0 || percent == "") {
+        console.log("Неверный percent");
+        return percent;
+    } else if (isNaN(contribution) || contribution < 0 || contribution == "") {
+        console.log("Неверный contribution");
+        return contribution;
+    } else if (isNaN(amount) || amount < 0 || amount == "") {
+        console.log("Неверный amount");
+        return amount;
+    } else if (isNaN(date) || date.getFullYear < new Date().getFullYear || date == "" || date < 0) {
+        console.log("Неверная data");
+        return date
+    }
     
-    let totalAmount = monthly_payment * n;
-    console.log(totalAmount.toFixed(2));
-    return totalAmount.toFixed(2);
+    else {
+        let s = amount - contribution; //сумма кредита - первонач взнос
+        let p = percent / 100 / 12;
+
+        let n = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
+    
+
+        let monthlyPayment = s * (p + (p / (Math.pow(1 + p, n) - 1)));
+    
+        let totalAmount = monthlyPayment * n;
+        console.log(totalAmount.toFixed(2));
+        return totalAmount.toFixed(2); 
+    }
+
 }
 
 
 function getGreeting(name) {
-    let greeting;
-    if (name === undefined || name === null || name === "") {
-        greeting = 'Привет, мир! Меня зовут Аноним.';
+    let greeting = 'Привет, мир! Меня зовут ';
+    if (name === undefined || name === null || name.trim() === "") {
+        greeting += 'Аноним.';
     } else {
-        greeting = `Привет, мир! Меня зовут ${name}.`;
+        greeting += `${name}.`;
     }
     console.log(greeting);
     return greeting;
